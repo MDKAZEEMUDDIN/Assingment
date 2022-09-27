f893fc4b13f263c6b6ea21c56e755be150a585ef
MDKAZEEMUDDIN <113545643+MDKAZEEMUDDIN@users.noreply.github.com>
2022-09-27 21:11:52 +0530

    Add files via upload

---
+++ CharecterExits/CharecterExits/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharecterExits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "kazeem";
+            char s2 = 'a';
+            Console.WriteLine(s1.Contains(s2));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 100; i++)
+            {
+                int c = 0;
+                for(int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                        c++;
+                }
+                if(c==2)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
